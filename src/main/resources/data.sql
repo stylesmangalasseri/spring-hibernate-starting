34c3d0fb36ff22c973e9067ede0e2e0a7b039d30
Styles.Mangalasseri <stylesmji@gmail.com>
2019-08-26 16:25:23 +0200

    Added .sql file to add default values or custom values to tables

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+----This is to insert values to DB
+--Eg: 
+--INSERT INTO  TABLE_NAME(COLUMN_NAME) VALUES('American');
+
+
+--INSERT INTO category (description) VALUES('American');
+--INSERT INTO category (description) VALUES('Italian');
+--INSERT INTO category (description) VALUES('Mexican');
+--INSERT INTO category (description) VALUES('Fast Food');
